b60ea359ff4e88d5f9d76fea945f726bc3d9acdb
crafty-alex <97107234+crafty-alex@users.noreply.github.com>
2022-10-26 12:02:04 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,13 @@
-public class HelloWorld {
+package com.alex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JusticeLeagueTrackerApplication {
+
     public static void main(String[] args) {
+        SpringApplication.run(HelloWorld.class, args);
         System.out.println("Hello World!");
     }
 }
