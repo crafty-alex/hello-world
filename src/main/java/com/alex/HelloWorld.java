048e376223b0d7088b8cfad7fb947b98f1a22188
crafty-alex <97107234+crafty-alex@users.noreply.github.com>
2022-10-28 14:57:07 +0200

    Update HelloWorld.java

--- src/main/java/com/alex/HelloWorld.java
+++ src/main/java/com/alex/HelloWorld.java
@@ -8,6 +8,5 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorld.class, args);
-        System.out.println("Hello World!");
     }
 }
