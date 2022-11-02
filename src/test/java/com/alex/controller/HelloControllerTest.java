94d1d0280f8f715f18be28dd919825fea6640737
crafty-alex <97107234+crafty-alex@users.noreply.github.com>
2022-11-02 09:48:09 +0100

    Create HelloControllerTest.java

---
+++ src/test/java/com/alex/controller/HelloControllerTest.java
@@ -0,0 +1,31 @@
+package com.alex.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.io.UnsupportedEncodingException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@ExtendWith({SpringExtension.class})
+@WebMvcTest(value=HelloController.class)
+class HelloControllerTest {
+  @Autowired
+  private MockMvc mvc;
+  
+  @Test
+  void when_get_hello_return_hello() throws Exception {
+    MvcResult result = mvc.perform(get("http://9191/hello"))
+            .andDo(print())
+            .andReturn();
+    String contentAsString = result.getResponse().getContentAsString();
+    assertEquals("Hello World", contentAsString);
+  }
+}
