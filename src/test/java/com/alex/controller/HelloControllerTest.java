0fa81c7f09215aca5ca468d58be5da524e0307e7
crafty-alex <97107234+crafty-alex@users.noreply.github.com>
2022-11-02 10:05:13 +0100

    Update HelloControllerTest.java

--- src/test/java/com/alex/controller/HelloControllerTest.java
+++ src/test/java/com/alex/controller/HelloControllerTest.java
@@ -26,6 +26,6 @@ class HelloControllerTest {
             .andDo(print())
             .andReturn();
     String contentAsString = result.getResponse().getContentAsString();
-    assertEquals("Hello Worldxxx", contentAsString);
+    assertEquals("Hello World", contentAsString);
   }
 }
